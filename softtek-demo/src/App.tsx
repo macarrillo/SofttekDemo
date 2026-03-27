ffd556ed71f84c67fa4d310d43bff40383445846
Marcos Carrillo <m.carrillo@hotmail.com>
2026-03-26 21:50:28 -0600

    Init

---
+++ softtek-demo/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Demo init</h1>
+          <p>
+            Softtek
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+    </>
+  )
+}
+
+export default App
